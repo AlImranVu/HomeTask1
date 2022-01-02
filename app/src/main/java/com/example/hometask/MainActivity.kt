1a0e85b5ca0c871200d05526ab748d6f46ec985d
Al Imran <alimran.vu@gmail.com>
2022-01-02 12:32:43 +0600

    ok

--- app/src/main/java/com/example/hometask/MainActivity.kt
+++ app/src/main/java/com/example/hometask/MainActivity.kt
@@ -2,26 +2,47 @@ package com.example.hometask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.message_view.*
 import kotlinx.android.synthetic.main.message_view.view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.lang.Exception
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
+    var baseCurrency = "EUR"
+    var convertedToCurrency = "EUR"
+    var conversionRate = 5f
+    var accessKey = "f50545120072aad1ab91ffe9f6d5e4fe"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
         val submit_btn = findViewById<TextView>(R.id.submit_btn)
 
 
+        spinnerSetup()
+
+
         submit_btn.setOnClickListener {
+
+
+            getApiResult()
+
+
 
 
             //Show Massage
@@ -30,14 +51,11 @@ class MainActivity : AppCompatActivity() {
             builder.setView(view)
 
 
-
             val dialog = builder.create()
             dialog.show()
             view.message_txt.setText("Bangladesh")
             dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
             dialog.setCancelable(false)
-
-
 
 
             view.txt_btn_done.setOnClickListener {
@@ -48,6 +66,121 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+
+
+
+    }
+
+    private fun getApiResult() {
+        if (et_firstConversion != null && et_firstConversion.text.isNotEmpty() && et_firstConversion.text.isNotBlank()) {
+
+            var API =
+                "http://api.exchangeratesapi.io/v1/latest?access_key=$accessKey&base=$baseCurrency&symbols=$convertedToCurrency"
+
+            if (baseCurrency == convertedToCurrency) {
+                Toast.makeText(
+                    applicationContext,
+                    "Please pick a currency to convert",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+
+                GlobalScope.launch(Dispatchers.IO) {
+
+                    try {
+
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        conversionRate =
+                            jsonObject.getJSONObject("rates").getString(convertedToCurrency)
+                                .toFloat()
+
+                        Log.d("Main", "$conversionRate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text =
+                                ((et_firstConversion.text.toString()
+                                    .toFloat()) * conversionRate).toString()
+
+                         //   ev_balance_view.setText(text)
+
+                        // et_secondConversion?.setText(text)
+
+                        }
+
+                    } catch (e: Exception) {
+                        Log.e("Main", "$e")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun spinnerSetup() {
+
+        val spinner: Spinner = findViewById(R.id.spinner_firstConversion)
+        val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
+
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currency_codes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinner.adapter = adapter
+
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currency_codes,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            spinner2.adapter = adapter
+
+        }
+
+
+        spinner.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                baseCurrency = parent?.getItemAtPosition(position).toString()
+
+            }
+
+        })
+
+
+        spinner2.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                convertedToCurrency = parent?.getItemAtPosition(position).toString()
+            }
+
+        })
+
 
     }
 }
